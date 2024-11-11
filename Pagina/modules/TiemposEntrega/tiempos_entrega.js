047c46ae85fdd06d01c7a703bb062514047ef8e7
Ricardo Madrigal <ricmad142@gmail.com>
2024-11-11 10:48:08 -0600

    Mas correcciones

--- Pagina/modules/TiemposEntrega/tiempos_entrega.js
+++ Pagina/modules/TiemposEntrega/tiempos_entrega.js
@@ -1,3 +1,31 @@
+function finalizarPedido() {
+    // Obtener datos de cotizador de localStorage
+    const cotizadorData = JSON.parse(localStorage.getItem('cotizadorData'));
+    const consumidor = document.getElementById('consumidor').value;
+    const contacto = document.getElementById('contacto').value;
+    
+    const pedidoData = {
+        id_cotizacion: document.getElementById('idCotizacion').value,
+        tecnica: cotizadorData.tecnica,
+        producto: cotizadorData.producto,
+        cantidad: cotizadorData.cantidad,
+        fecha_requerida: cotizadorData.fecha_requerida,
+        costo: cotizadorData.costo,
+        consumidor: consumidor,
+        contacto: contacto
+    };
+    
+    // Guardar datos en localStorage
+    let pedidosActivos = JSON.parse(localStorage.getItem('pedidosActivos')) || [];
+    pedidosActivos.push(pedidoData);
+    localStorage.setItem('pedidosActivos', JSON.stringify(pedidosActivos));
+    
+    
+    // Redirigir a pedidos activos
+    window.location.href = '../Pedidos Activos/Activos.html';
+
+}
+
 function generarID() {
     // Genera un número aleatorio de 6 dígitos
     const id = Math.floor(100000 + Math.random() * 900000);
@@ -16,28 +44,4 @@ function closeNav() {
     document.getElementById("mySidebar").classList.remove("open");
 }
 
-function finalizarPedido() {
-    // Obtener datos de cotizador de localStorage
-    const cotizadorData = JSON.parse(localStorage.getItem('cotizadorData'));
-    const consumidor = document.getElementById('consumidor').value;
-    const contacto = document.getElementById('contacto').value;
 
-    const pedidoData = {
-        id_cotizacion: document.getElementById('id_cotizacion').value,
-        tecnica: cotizadorData.tecnica,
-        producto: cotizadorData.producto,
-        cantidad: cotizadorData.cantidad,
-        fecha_requerida: cotizadorData.fecha_requerida,
-        costo: cotizadorData.costo,
-        consumidor: consumidor,
-        contacto: contacto
-    };
-
-    // Guardar datos en localStorage
-    let pedidosActivos = JSON.parse(localStorage.getItem('pedidosActivos')) || [];
-    pedidosActivos.push(pedidoData);
-    localStorage.setItem('pedidosActivos', JSON.stringify(pedidosActivos));
-
-    // Redirigir a pedidos activos
-    window.location.href = '../Pedidos Activos/Activos.html';
-}
