d622f4c3510057a1a1ffbbdd3c9f244daba212f4
Irene Maldonado <91904234+IreneAMaldonado03@users.noreply.github.com>
2024-11-13 09:00:15 -0600

    Funcionalidades de tiempo de entrega

--- Pagina/modules/TiemposEntrega/tiempos_entrega.js
+++ Pagina/modules/TiemposEntrega/tiempos_entrega.js
@@ -1,16 +1,15 @@
-function finalizarPedido() {
-    // Obtener datos de cotizador de localStorage
+// Obtener datos de cotizador de localStorage
     const cotizadorData = JSON.parse(localStorage.getItem('cotizadorData'));
     const consumidor = document.getElementById('consumidor').value;
     const contacto = document.getElementById('contacto').value;
-    
+    const idCotizacion = localStorage.getItem('cotizacionID');
+
     const pedidoData = {
-        id_cotizacion: document.getElementById('idCotizacion').value,
+        id_cotizacion: document.getElementById('idCotizacion').textContent=idCotizacion,
         tecnica: cotizadorData.tecnica,
         producto: cotizadorData.producto,
         cantidad: cotizadorData.cantidad,
         fecha_requerida: cotizadorData.fecha_requerida,
-        costo: cotizadorData.costo,
         consumidor: consumidor,
         contacto: contacto
     };
@@ -26,14 +25,79 @@ function finalizarPedido() {
 
 }
 
+
+function fPedido(){
+    const dataPedido = {
+        consumidor: document.getElementById ('consumidor').value,
+        contacto: document.getElementById ('contacto').value,
+        descripcion : document.getElementById ('descripcion').value,
+        idCotizacion : document.getElementById ('idCotizacion').value
+    }
+
+    
+    
+
+    const cotizadorDataString = localStorage.getItem('cotizadorData');
+    const pedidoDataString = localStorage.getItem('dataPedido');
+    const cotizadorData = JSON.parse(cotizadorDataString);
+    const pedidoData = JSON.parse(pedidoDataString);
+   
+    document.getElementById('idCotizacion').textContent=dataPedido.idCotizacion;
+    document.getElementById('cantidad').textContent=cotizadorData.cantidad;
+    document.getElementById('tecnica').textContent=cotizadorData.tecnica;
+    document.getElementById('producto').textContent=cotizadorData.tecnica;
+    document.getElementById('cantidad').textContent=cotizadorData.cantidad;
+    
+
+    localStorage.setItem('dataPedido', JSON.stringify(dataPedido))
+    
+
+    window.location.href = '../Pedidos Activos/Activos.html';
+    
+}
+
+
+
+// Ejecuta la función al cargar la página
+window.onload = recarga;
+
+function recarga(){
+    generarID();
+    dataCotizador();
+}
+
 function generarID() {
     // Genera un número aleatorio de 6 dígitos
     const id = Math.floor(100000 + Math.random() * 900000);
     document.getElementById("idCotizacion").textContent = id;
+    cotizadorData.idCotizacion = id;
+    localStorage.setItem('cotizacionID',id);
 }
 
-// Ejecuta la función al cargar la página
-window.onload = generarID;
+function dataCotizador() {
+    const cotizadorDataString = localStorage.getItem('cotizadorData')
+    const cotizadorData = JSON.parse(cotizadorDataString);
+    const cantidadTA = cotizadorData.cantidad;
+    const valorBase = 2;
+    const factorIncr = 0.5;
+    const tiempoA = valorBase * (cantidadTA * factorIncr)
+    const pedido ={
+        idCotizacion:cotizadorData.idCotizacion,
+        fecha_requerida: cotizadorData.fecha_requerida,
+        producto: cotizadorData.producto,
+    };
+
+    let pedidosActivos = JSON.parse(localStorage.getItem('pedidosActivos')) || [];
+    pedidosActivos.push(pedido);
+    localStorage.setItem('pedidosActivos', JSON.stringify(pedidosActivos));
+
+    document.getElementById('tecnica').textContent=cotizadorData.tecnica;
+    document.getElementById('cantidad').textContent=cotizadorData.cantidad;
+    document.getElementById('producc').textContent=tiempoA;
+
+    window.location.href = '../Pedidos Activos/Activos.html';
+}
+
 
 
 function openNav() {
